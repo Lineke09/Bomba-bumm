516e924e17681c5315d344f5be69bd9cafc985bc
Lineke09 <linekefc@gmail.com>
2022-07-26 16:47:22 -0300

    first commit

---
+++ import time bomba.py
@@ -0,0 +1,8 @@
+import time
+tempoInicial = 20
+tempoFinal = 0
+print("inciando contagem regressiva")
+for i in range(tempoInicial,tempoFinal-1):
+ print("i""segundos")
+time.sleep(1)
+print("BUM!")
